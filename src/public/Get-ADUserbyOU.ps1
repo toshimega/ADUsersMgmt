05c7517dcefae9ebe72dcf6b1894a2e87a95b15e
Rui Nogueira <Rui.MF.Nogueira@azores.gov.pt>
2022-07-28 10:49:21 +0000

    refactor: changed Get-ADUserbyOU params to use only function parameter and no hardcoded values

--- src/public/Get-ADUserbyOU.ps1
+++ src/public/Get-ADUserbyOU.ps1
@@ -2,46 +2,42 @@ function Get-ADUserbyOU {
     [cmdletbinding()]
 
     param (
-        [parameter(ValueFromPipeline,
+        [parameter(
+            ValueFromPipeline,
             ValueFromPipelinebyPropertyName)]
-        [ValidateSet('Students', 'Teachers', 'Employees')]
         [ValidateNotNullorEmpty()]
-        [string]
-        $OrganizationalUnit,
-        
-        [Switch]
-        $All
+        [string[]]$ou_distinguishedname
     )
     BEGIN {
-        Write-Verbose "[BEGIN]Starting $($MyInvocation.MyCommand)"
+        Write-Verbose "[BEGIN]  Starting $($MyInvocation.MyCommand)"
+
         $params = @{
             'Filter'     = '*'
-            'Properties' = 'CN', 'DistinguishedName', 'EmployeeType', 'Enabled', 'GivenName', 'Mail', 'Name', 'ObjectClass', 'ObjectGUID', 'SamAccountName', 'SID', 'Surname', 'UserPrincipalName', 'WhenChanged', 'WhenCreated'
+            'Properties' = 
+            'CN',
+            'DistinguishedName',
+            'EmployeeType',
+            'Enabled',
+            'GivenName',
+            'Mail',
+            'Name',
+            'ObjectClass',
+            'ObjectGUID',
+            'SamAccountName',
+            'SID',
+            'Surname',
+            'UserPrincipalName',
+            'WhenChanged',
+            'WhenCreated'
+            'searchbase' = "$($ou_distinguishedname)"
         }
     }
     PROCESS {
-        if ($All) {
-            $searchbase = 'OU=4_Alunos,DC=edu,DC=azores,DC=gov,DC=local', 'OU=5_Docentes,DC=edu,DC=azores,DC=gov,DC=local', 'OU=6_Colaboradores,DC=edu,DC=azores,DC=gov,DC=local'
-            foreach ($ou in $searchbase) {
-                Get-ADUser @params -SearchBase "$($ou)"
-            }
-        }
-        else {
-            switch -Regex ($OrganizationalUnit) {
-                'Alunos' {
-                    $params.Add('Searchbase', 'OU=4_Students,DC=contoso,DC=com'); Break 
-                }
-                'Docentes' {
-                    $params.Add('Searchbase', 'OU=5_Teachers,DC=contoso,DC=com'); Break 
-                }
-                'Colaboradores' {
-                    $params.Add('Searchbase', 'OU=6_Employees,DC=contoso,DC=com'); Break 
-                }
-            }
+        foreach ($ou in $searchbase) {
             Get-ADUser @params
         }
+
     }
     END {
     }      
-} #Get-ADUserbyOU
-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
+}
--- tests/Get-ADUserbyOU.ps1
+++ tests/Get-ADUserbyOU.ps1
@@ -1,46 +1,37 @@
-function Get-ADUserbyOU {
+function Get-ADUsersEduBox {
     [cmdletbinding()]
 
     param (
         [parameter(ValueFromPipeline,
             ValueFromPipelinebyPropertyName)]
-        [ValidateSet('Alunos', 'Docentes', 'Colaboradores')]
         [ValidateNotNullorEmpty()]
         [string]
-        $OrganizationalUnit,
-        
-        [Switch]
-        $All
+        $ou_distinguishedname
     )
     BEGIN {
-        Write-Verbose "[BEGIN]Starting $($MyInvocation.MyCommand)"
-        $params = @{
-            'Filter'     = '*'
-            'Properties' = 'CN', 'DistinguishedName', 'EmployeeType', 'Enabled', 'GivenName', 'Mail', 'Name', 'ObjectClass', 'ObjectGUID', 'SamAccountName', 'SID', 'Surname', 'UserPrincipalName', 'WhenChanged', 'WhenCreated'
-        }
+        Write-Verbose "[BEGIN]  Starting $($MyInvocation.MyCommand)"
+
+        
     }
     PROCESS {
-        if ($All) {
-            $searchbase = 'OU=4_Alunos,DC=edu,DC=azores,DC=gov,DC=local', 'OU=5_Docentes,DC=edu,DC=azores,DC=gov,DC=local', 'OU=6_Colaboradores,DC=edu,DC=azores,DC=gov,DC=local'
+        try {
+            if ($ou_distinguishedname) {
+
+            }
+            $searchbase = 
+            'OU=4_Alunos,DC=edu,DC=azores,DC=gov,DC=local',
+            'OU=5_Docentes,DC=edu,DC=azores,DC=gov,DC=local',
+            'OU=6_Colaboradores,DC=edu,DC=azores,DC=gov,DC=local'
+            
             foreach ($ou in $searchbase) {
                 Get-ADUser @params -SearchBase "$($ou)"
             }
         }
-        else {
-            switch -Regex ($OrganizationalUnit) {
-                'Alunos' {
-                    $params.Add('Searchbase', 'OU=4_Alunos,DC=edu,DC=azores,DC=gov,DC=local'); Break 
-                }
-                'Docentes' {
-                    $params.Add('Searchbase', 'OU=5_Docentes,DC=edu,DC=azores,DC=gov,DC=local'); Break 
-                }
-                'Colaboradores' {
-                    $params.Add('Searchbase', 'OU=6_Colaboradores,DC=edu,DC=azores,DC=gov,DC=local'); Break 
-                }
-            }
-            Get-ADUser @params
-        }
+        catch {
+            $_
+        }   
     }
     END {
+        Write-Verbose "[END]    Ending $($MyInvocation.MyCommand)"
     }      
 }
